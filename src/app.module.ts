82de9b14d660bf4c5cc9ba8eb7f0b197536699dc
aabdalnaby088 <11410120210035@stud.cu.edu.eg>
2025-05-13 16:33:59 +0300

    db hosted on mongo atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,17 @@ import { CartModule } from './Cart/cart.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/Areeb'),
     UserModule,
     EventModule,
     CartModule,
     ConfigModule.forRoot({ isGlobal: true }), 
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule], 
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DB_URI'), 
+      }),
+      inject: [ConfigService], 
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
